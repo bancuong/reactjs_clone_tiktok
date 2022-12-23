80d68f493f7a4d0fdba28b3f7d03e3f4be845d23
cuongnd <nguyenlongan040100@gmail.com>
2022-12-23 08:14:38 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,22 @@
 const express = require("express");
+const { chats } = require("./data/data");
+
 const app = express();
-app.listen(5000, console.log("Server start on port 5000"));
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Server start on port: ", PORT));
